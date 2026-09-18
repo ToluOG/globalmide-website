c41f213b58bac413830ace06535422d846100aa7
ToluOG <onitoluwalope95@gmail.com>
2026-09-17 21:55:28 -0600

    Fix resizig navbar issuer

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('nav-open');
   hamburger.classList.toggle('hamburger-active');
 });
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    navLinks.classList.remove('nav-open');
+    hamburger.classList.remove('hamburger-active');
+  }
+});
