27f0c3afc6a89cfedef5401c50bbbbd3b4eb0b0b
ToluOG <onitoluwalope95@gmail.com>
2026-09-17 20:05:06 -0600

    Navbar Transmission

--- Js/script.js
+++ Js/script.js
@@ -3,4 +3,5 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('nav-open');
+  hamburger.classList.toggle('hamburger-active');
 });
